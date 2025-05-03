3a080f5377236a38483b6cbd659cdd633596e553
Omer Tairi <tairiomer04@gmail.com>
2025-05-03 14:04:00 +0200

    hello

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -101,7 +101,7 @@ const Index = () => {
 
         return matchesTab && (searchQuery === "" || matchesSearch);
     });
-
+    console.log("sfdsfjjdsflsjgklsjgkljgkljdklf")
     console.log("JEREEEE")
 
     return (
